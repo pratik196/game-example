07646de6a29d7652f1a547c92a5b688e445173a9
Pratik <pratikdhole196@gmail.com>
2021-01-05 11:45:58 +0530

    Project

---
+++ Sample/Assets/Script/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMov movement;
+    public GameManager gameManager;
+  void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
